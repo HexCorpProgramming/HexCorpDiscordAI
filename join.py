69a0c6d4f92470f4c4619ad6758a898e623b7136
Drone #9813 <9813@hexcorp.com>
2020-01-12 15:39:46 +0100

    prepared bot for use on multiple guilds

--- join.py
+++ join.py
@@ -1,23 +1,18 @@
 from discord.ext import commands
+from discord.utils import get
 import discord
-import random
 
-INITIATE_ROLE = 'Initiate'
+INITIATE = 'Initiate'
+ASSOCIATE = 'Associate'
 
 class Join(commands.Cog):
-    ''' This Cog listens for a new member joining the channel and assigns them the role Initiate'''
+    ''' This Cog listens for a new member joining the channel and assigns them the role Initiate. '''
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_ready(self):
-        for role in self.bot.guilds[0].roles:
-            if role.name == INITIATE_ROLE:
-                self.initiate_role = role
-                return
-
-    @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        member.add_roles(self.initiate_role)
+        initiate_role = get(member.guild.roles, name=INITIATE)
+        member.add_roles(initiate_role)
         
