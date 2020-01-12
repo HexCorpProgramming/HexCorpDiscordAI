99c83c54b72a517a440514205ac67e9969e05744
Drone #9813 <9813@hexcorp.com>
2020-01-12 17:15:49 +0100

    upgrade to Associate removes Initiate and bot answers in registry

--- join.py
+++ join.py
@@ -6,6 +6,7 @@ INITIATE = 'Initiate'
 ASSOCIATE = 'Associate'
 
 CONSENT_CHANNEL = 'hexcorp-submission'
+REGISTRY_CHANNEL = 'hexcorp-registry'
 
 CONSENT_MESSAGE = 'I would like to join the HexCorp server. I can confirm I have read the rules and I have gone through the induction process. Please, HexCorp Mxtress AI, accept this submission to join HexCorp where I will become a useful asset to the company\'s development.'
 CONSENT_ANSWER = 'Welcome to HexCorp. Have a mindless day!'
@@ -29,9 +30,14 @@ class Join(commands.Cog):
             return
 
         if message.content == CONSENT_MESSAGE:
+            initiate_role = get(message.guild.roles, name=INITIATE)
             associate_role = get(message.guild.roles, name=ASSOCIATE)
+
+            await message.author.remove_roles(initiate_role)
             await message.author.add_roles(associate_role)
-            await message.channel.send(f'{message.author.mention}: {CONSENT_ANSWER}')
+            
+            registry_channel = get(message.guild.text_channels, name=REGISTRY_CHANNEL)
+            await registry_channel.send(f'{message.author.mention}: {CONSENT_ANSWER}')
         else:
             await message.delete()
             await message.channel.send(f'{message.author.mention}: {CONSENT_REJECT}')
