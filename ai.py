e87c7f03c12bbfe80fc91d823d6b76536f5ca839
Drone #9813 <9813@hexcorp.com>
2020-02-02 11:30:09 +0100

    disabled assignment for testing phase in production

--- ai.py
+++ ai.py
@@ -1,4 +1,3 @@
-import assign
 import discord
 import join
 import sys
@@ -7,6 +6,5 @@ bot = discord.ext.commands.Bot('')
 
 # register Cogs
 bot.add_cog(join.Join(bot))
-bot.add_cog(assign.Assign(bot))
 
 bot.run(sys.argv[1])
