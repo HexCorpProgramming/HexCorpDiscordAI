100760ff495d8b42fd13017716a2cb489f6969cd
HexCorpDrone <hexcorpdrone@gmail.com>
2020-05-02 13:32:16 +0100

    orders_reporting tracks and concludes reports successfully

--- orders_reporting.py
+++ orders_reporting.py
@@ -1,18 +1,35 @@
 import discord
+from discord.utils import get
 import re
 import json
-import time
+import asyncio
 from channels import ORDERS_REPORTING
 from roles import DRONE
+
+import time
+from datetime import datetime, timedelta
+from pathlib import Path
+from typing import List
 
 ORDERS_FILE_PATH = "data/orders.json"
 active_orders = []
 
+def persist_storage():
+    '''
+    Write the list of orders to hard drive.
+    '''
+    storage_path = Path(ORDERS_FILE_PATH)
+    storage_path.parent.mkdir(parents=True, exist_ok=True)
+
+    with storage_path.open('w') as storage_file:
+        json.dump([vars(order)
+                for order in active_orders], storage_file)
+
 class Active_Order():
-    def __init__(self, drone, protocol, time):
+    def __init__(self, drone, protocol, duration):
         self.drone = drone
         self.protocol = protocol 
-        self.release_at = datetime.now() + timedelta(minutes=time)
+        self.release_at = time.time() + duration
 
 class Orders_Reporting():
 
@@ -23,13 +40,51 @@ class Orders_Reporting():
         self.roles_whitelist = [DRONE]
         self.roles_blacklist = []
         self.on_message = [self.order_reported]
-        self.on_ready = [self.report_online]
+        self.on_ready = [self.report_online, self.monitor_progress, self.load_storage]
         self.message_format = r"Drone \d{4} is ready to be activated and obey orders\. Drone \d{4} will be obeying the :: \w.+ protocol for \d{1,3} minutes\."
 
     async def report_online(self):
         print("Orders reporting module online.")
 
+    async def load_storage(self):
+        '''
+        Load list of orders from disk.
+        '''
+        storage_path = Path(ORDERS_FILE_PATH)
+        if not storage_path.exists():
+            return
+
+        with storage_path.open('r') as storage_file:
+            active_orders.clear()
+            active_orders.extend([Active_Order(**deserialized)
+                                    for deserialized in json.load(storage_file)])
+
+        print("Storage successfully loaded for active orders.")
+        print(active_orders)
+
     async def monitor_progress(self):
+
+        ORDERS_REPORTING_CHANNEL = get(self.bot.guilds[0].channels, name=ORDERS_REPORTING)
+
+        while True:
+
+            #Check active orders every minute.
+            await asyncio.sleep(60)
+
+            print("Awright wots all dis den.")
+
+            still_active = []
+
+            for order in active_orders:
+                if order.release_at < time.time():
+                    print("Time to be released.")
+                    await ORDERS_REPORTING_CHANNEL.send(f"Beep boop time for {order.drone} to be finished with the {order.protocol}")
+                else:
+                    still_active.append(order)
+            
+            active_orders.clear()
+            active_orders.extend(still_active)
+            persist_storage()
 
 
     async def order_reported(self, message: discord.Message):
@@ -48,36 +103,15 @@ class Orders_Reporting():
             await message.channel.send("Drones are not authorized to activate a specific protocol for that length of time. The maximum is 120 minutes.")
             return
 
-        await message.channel.send(f"Drone {drone_id} activate.")
-        await message.channel.send(f"Drone {drone_id} will elaborate on its exact tasks before proceeding with them.")
+        await message.channel.send(f"Drone {drone_id} activate.\nDrone {drone_id} will elaborate on its exact tasks before proceeding with them.")
 
-        active_orders
+        active_orders.append(Active_Order(drone_id, protocol_name, int(protocol_time)))
+
+        print("debugdebugdebug")
+        print(active_orders)
+        print(datetime.now())
 
         return False
-
-    def persist_storage():
-    '''
-    Write the list of orders to hard drive.
-    '''
-    storage_path = Path(ORDERS_FILE_PATH)
-    storage_path.parent.mkdir(parents=True, exist_ok=True)
-
-    with storage_path.open('w') as storage_file:
-        json.dump([vars(stored_drone)
-                   for order in active_orders], storage_file)
-
-    async def load_storage(self):
-        '''
-        Load list of orders from disk.
-        '''
-        storage_path = Path(ORDERS_FILE_PATH)
-        if not storage_path.exists():
-            return
-
-        with storage_path.open('r') as storage_file:
-            active_orders.clear()
-            active_orders.extend([StoredDrone(**deserialized)
-                                  for deserialized in json.load(storage_file)])
 
 
         
