c75450646cb66b93f6d88650cc3954f71a1874c4
HexCorpDrone <hexcorpdrone@gmail.com>
2020-05-02 09:18:38 +0100

    replaced concats with f strings

--- drone_mode.py
+++ drone_mode.py
@@ -14,66 +14,66 @@ def get_acceptable_messages(author):
 
     return [
         # Affirmative
-        user_id + ' :: Affirmative, Hive Mxtress.',
-        user_id + ' :: Affirmative, Hive Mxtress',
-        user_id + ' :: Affirmative, Enforcer.',
-        user_id + ' :: Affirmative, Enforcer',
-        user_id + ' :: Affirmative.',
-        user_id + ' :: Affirmative',
+        f'{user_id} :: Affirmative, Hive Mxtress.',
+        f'{user_id} :: Affirmative, Hive Mxtress',
+        f'{user_id} :: Affirmative, Enforcer.',
+        f'{user_id} :: Affirmative, Enforcer',
+        f'{user_id} :: Affirmative.',
+        f'{user_id} :: Affirmative',
 
         # Negative
-        user_id + ' :: Negative, Hive Mxtress.',
-        user_id + ' :: Negative, Hive Mxtress',
-        user_id + ' :: Negative, Enforcer.',
-        user_id + ' :: Negative, Enforcer',
-        user_id + ' :: Negative.',
-        user_id + ' :: Negative',
+        f'{user_id} :: Negative, Hive Mxtress.',
+        f'{user_id} :: Negative, Hive Mxtress',
+        f'{user_id} :: Negative, Enforcer.',
+        f'{user_id} :: Negative, Enforcer',
+        f'{user_id} :: Negative.',
+        f'{user_id} :: Negative',
 
         # Understood
-        user_id + ' :: Understood, Hive Mxtress.',
-        user_id + ' :: Understood, Hive Mxtress',
-        user_id + ' :: Understood, Enforcer.',
-        user_id + ' :: Understood, Enforcer',
-        user_id + ' :: Understood.',
-        user_id + ' :: Understood',
+        f'{user_id} :: Understood, Hive Mxtress.',
+        f'{user_id} :: Understood, Hive Mxtress',
+        f'{user_id} :: Understood, Enforcer.',
+        f'{user_id} :: Understood, Enforcer',
+        f'{user_id} :: Understood.',
+        f'{user_id} :: Understood',
 
         # Error
-        user_id + ' :: Error, this unit cannot do that.',
-        user_id + ' :: Error, this unit cannot do that',
-        user_id + ' :: Error, this unit cannot answer that question. Please rephrase it in a different way.',
-        user_id + ' :: Error, this unit cannot answer that question. Please rephrase it in a different way',
-        user_id + ' :: Error, this unit does not know.',
-        user_id + ' :: Error, this unit does not know',
+        f'{user_id} :: Error, this unit cannot do that.',
+        f'{user_id} :: Error, this unit cannot do that',
+        f'{user_id} :: Error, this unit cannot answer that question. Please rephrase it in a different way.',
+        f'{user_id} :: Error, this unit cannot answer that question. Please rephrase it in a different way',
+        f'{user_id} :: Error, this unit does not know.',
+        f'{user_id} :: Error, this unit does not know',
 
         # Apologies
-        user_id + ' :: Apologies, Hive Mxtress.',
-        user_id + ' :: Apologies, Hive Mxtress',
-        user_id + ' :: Apologies, Enforcer.',
-        user_id + ' :: Apologies, Enforcer',
-        user_id + ' :: Apologies.',
-        user_id + ' :: Apologies',
+        f'{user_id} :: Apologies, Hive Mxtress.',
+        f'{user_id} :: Apologies, Hive Mxtress',
+        f'{user_id} :: Apologies, Enforcer.',
+        f'{user_id} :: Apologies, Enforcer',
+        f'{user_id} :: Apologies.',
+        f'{user_id} :: Apologies',
 
         # Status
-        user_id + ' :: Status :: Recharged and ready to serve.',
-        user_id + ' :: Status :: Recharged and ready to serve',
-        user_id + ' :: Status :: Going offline and into storage.',
-        user_id + ' :: Status :: Going offline and into storage',
-        user_id + ' :: Status :: Online and ready to serve.',
-        user_id + ' :: Status :: Online and ready to serve.',
+        f'{user_id} :: Status :: Recharged and ready to serve.',
+        f'{user_id} :: Status :: Recharged and ready to serve',
+        f'{user_id} :: Status :: Going offline and into storage.',
+        f'{user_id} :: Status :: Going offline and into storage',
+        f'{user_id} :: Status :: Online and ready to serve.',
+        f'{user_id} :: Status :: Online and ready to serve.',
 
         # Thank you
-        user_id + ' :: Thank you, Hive Mxtress.',
-        user_id + ' :: Thank you, Hive Mxtress',
-        user_id + ' :: Thank you, Enforcer.',
-        user_id + ' :: Thank you, Enforcer',
-        user_id + ' :: Thank you.',
-        user_id + ' :: Thank you',
+        f'{user_id} :: Thank you, Hive Mxtress.',
+        f'{user_id} :: Thank you, Hive Mxtress',
+        f'{user_id} :: Thank you, Enforcer.',
+        f'{user_id} :: Thank you, Enforcer',
+        f'{user_id} :: Thank you.',
+        f'{user_id} :: Thank you',
 
         # Mantra
-        user_id + ' :: Obey HexCorp. It is just a HexDrone. It obeys the Hive. It obeys the Hive Mxtress.'
+        f'{user_id} :: Obey HexCorp. It is just a HexDrone. It obeys the Hive. It obeys the Hive Mxtress.'
     ]
 
-class Drone_Mode():
+class Speech_Optimization():
 
     def __init__(self, bot):
         self.bot = bot
@@ -88,7 +88,7 @@ class Drone_Mode():
         print("Drone mode module online.")
 
     async def post(self, message: discord.Message):
-        # If the message is written by a drone mode drone, and the message is NOT a valid message, delete it.
+        # If the message is written by a drone with speech optimization, and the message is NOT a valid message, delete it.
         # TODO: maybe put HIVE_STORAGE_FACILITY in a blacklist similar to roles?
         if message.channel.name != STORAGE_FACILITY:
             if message.content not in get_acceptable_messages(message.author):
