76cf20f1948087355d16f297d6696bb38814d2d4
Drone #9813 <9813@hexcorp.net>
2020-05-08 14:19:38 +0200

    fixed json deserialization of orders

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 ## [Unreleased]
 ### Fixed
 - list of orders is persisted when a new order is added
+- list of orders was not deserialized properly
 
 ## [1.2.0.2]
 ### Fixed
--- orders_reporting.py
+++ orders_reporting.py
@@ -18,6 +18,7 @@ LOGGER = logging.getLogger('ai')
 ORDERS_FILE_PATH = "data/orders.json"
 active_orders = []
 
+
 def persist_storage():
     '''
     Write the list of orders to hard drive.
@@ -27,14 +28,16 @@ def persist_storage():
 
     with storage_path.open('w') as storage_file:
         json.dump([vars(order)
-                for order in active_orders], storage_file)
+                   for order in active_orders], storage_file)
+
 
 class Active_Order():
-    def __init__(self, drone, user_id, protocol, duration):
+    def __init__(self, drone, user_id, protocol, release_at):
         self.drone = drone
         self.user_id = user_id
-        self.protocol = protocol 
-        self.release_at = time.time() + (duration * 60)
+        self.protocol = protocol
+        self.release_at = release_at
+
 
 class Orders_Reporting():
 
@@ -45,7 +48,8 @@ class Orders_Reporting():
         self.roles_whitelist = [DRONE]
         self.roles_blacklist = []
         self.on_message = [self.order_reported]
-        self.on_ready = [self.report_online, self.monitor_progress, self.load_storage]
+        self.on_ready = [self.report_online,
+                         self.monitor_progress, self.load_storage]
         self.monitor_progress_started = False
         self.message_format = r"Drone \d{4} is ready to be activated and obey orders\. Drone \d{4} will be obeying the :: \w.+ protocol for \d{1,3} (minutes|minute)\."
 
@@ -63,7 +67,7 @@ class Orders_Reporting():
         with storage_path.open('r') as storage_file:
             active_orders.clear()
             active_orders.extend([Active_Order(**deserialized)
-                                    for deserialized in json.load(storage_file)])
+                                  for deserialized in json.load(storage_file)])
 
         LOGGER.debug("Storage successfully loaded for active orders.")
         LOGGER.debug(f"Current orders: {active_orders}")
@@ -73,21 +77,22 @@ class Orders_Reporting():
             return
 
         self.monitor_progress_started = True
-        ORDERS_REPORTING_CHANNEL = get(self.bot.guilds[0].channels, name=ORDERS_REPORTING)
+        ORDERS_REPORTING_CHANNEL = get(
+            self.bot.guilds[0].channels, name=ORDERS_REPORTING)
         while True:
-            #Check active orders every minute.
+            # Check active orders every minute.
             await asyncio.sleep(60)
             LOGGER.debug("Checking for completed orders")
             still_active = []
             for order in active_orders:
                 if order.release_at < time.time():
 
-                    #get the drone responsible
+                    # get the drone responsible
                     user = get(self.bot.guilds[0].members, id=order.user_id)
                     await ORDERS_REPORTING_CHANNEL.send(f"{user.mention} Drone {order.drone} Deactivate.\nDrone {order.drone}, good drone.")
                 else:
                     still_active.append(order)
-            
+
             active_orders.clear()
             active_orders.extend(still_active)
             persist_storage()
@@ -101,7 +106,8 @@ class Orders_Reporting():
 
         drone_id = re.search(r"\d{4}", message.content).group()
 
-        drone_id_from_user = re.search(r"\d{4}", message.author.display_name).group()
+        drone_id_from_user = re.search(
+            r"\d{4}", message.author.display_name).group()
 
         if drone_id != drone_id_from_user:
             await message.channel.send("Your drone ID does not match the declaration.")
@@ -114,17 +120,18 @@ class Orders_Reporting():
 
         protocol_name = re.search(r":: \w.+ protocol", message.content).group()
         protocol_name = protocol_name[3:]
-        protocol_time = re.search(r"(?<!\d)\d{1,3}(?!\d)", message.content).group()
+        protocol_time = re.search(
+            r"(?<!\d)\d{1,3}(?!\d)", message.content).group()
 
         if int(protocol_time) > 120:
             await message.channel.send("Drones are not authorized to activate a specific protocol for that length of time. The maximum is 120 minutes.")
             return
 
         await message.channel.send(f"Drone {drone_id} Activate.\nDrone {drone_id} will elaborate on its exact tasks before proceeding with them.")
-        active_orders.append(Active_Order(drone_id, message.author.id, protocol_name, int(protocol_time)))
+        active_orders.append(Active_Order(
+            drone_id, message.author.id, protocol_name, time.time() + (int(protocol_time) * 60)))
 
         LOGGER.debug(f"Current orders: {active_orders}")
         persist_storage()
 
         return False
-
