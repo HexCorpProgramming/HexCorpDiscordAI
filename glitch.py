81c32f917d3a2efb95ab074a9e0654817beae053
Drone #9813 <9813@hexcorp.net>
2020-06-08 11:54:32 +0200

    all messages sent over webhooks are now glitched if a drone has the role

--- glitch.py
+++ glitch.py
@@ -1,4 +1,7 @@
+import discord
 import random
+from roles import GLITCHED, has_role
+import re
 
 combining_characters = list(range(0x0300, 0x036F))
 
@@ -11,3 +14,15 @@ def glitch(text: str):
             glitched_text += chr(random.choice(combining_characters))
 
     return glitched_text
+
+glitch_template = re.compile(r'(\d{4} :: )(.*)')
+
+def glitch_if_applicable(text: str, author: discord.Member):
+    if has_role(author, GLITCHED):
+        template_match = glitch_template.match(text)
+        if template_match:
+            return template_match.group(1) + glitch(template_match.group(2))
+        else:
+            return glitch(text)
+    else:
+        return text
