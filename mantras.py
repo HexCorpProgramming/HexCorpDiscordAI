d2f6b56c898aa59311e50495af165b642ae06a0b
HexCorpDrone <hexcorpdrone@gmail.com>
2020-07-01 17:32:35 +0100

    Changed mantra module permissions

--- mantras.py
+++ mantras.py
@@ -3,7 +3,7 @@ import logging
 import os
 from discord.utils import get
 from channels import REPETITIONS
-from roles import HIVE_MXTRESS, MODERATION
+from roles import HIVE_MXTRESS
 
 class Mantras():
 
@@ -50,14 +50,14 @@ class Mantras():
             topic_message = message.content.replace("Repeat :: ", "Repeat :: [ID] :: ")
             await message.channel.edit(topic=topic_message) #Update the channel description
             self.LOGGER.info(f"Mantra updated and written to file: {Mantras.current_mantra}")
-            
+
         return True
 
     def __init__(self, bot):
         self.bot = bot
         self.channels_whitelist = [REPETITIONS]
         self.channels_blacklist = []
-        self.roles_whitelist = [MODERATION]
+        self.roles_whitelist = [HIVE_MXTRESS]
         self.roles_blacklist = []
         self.on_message = [self.update_mantra]
         self.on_ready = [self.load_mantra]
