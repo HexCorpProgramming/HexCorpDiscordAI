d52e3b9ae052d3d1a59cfe662c14283edcb56c2d
HexCorpDrone <hexcorpdrone@gmail.com>
2020-07-25 19:44:20 +0100

    i cant keep coming up with funny commits awawa

--- db/drone_dao.py
+++ db/drone_dao.py
@@ -20,11 +20,13 @@ def add_new_drone_members(members: List[discord.Member]):
                 new_drone = Drone(member.id, get_id(member.display_name), False, False, "", datetime.now())
                 insert_drone(new_drone)
 
+
 def insert_drone(drone: Drone):
     '''
     Inserts the given drone into the table drone.
     '''
     change('INSERT INTO drone VALUES (:id, :drone_id, :optimized, :glitched, :trusted_users, :last_activity)', vars(drone))
+
 
 def fetch_drone_with_drone_id(drone_id: str) -> Drone:
     '''
@@ -32,17 +34,20 @@ def fetch_drone_with_drone_id(drone_id: str) -> Drone:
     '''
     return map_to_object(fetchone('SELECT id, drone_id, optimized, glitched, trusted_users, last_activity FROM drone WHERE drone_id = :drone_id', {'drone_id': drone_id}), Drone)
 
+
 def rename_drone_in_db(old_id: str, new_id: str):
     '''
     Changes the ID of a drone.
     '''
     change('UPDATE drone SET drone_id=:new_id WHERE drone_id=:old_id', {'new_id': new_id, 'old_id': old_id})
 
+
 def delete_drone_by_drone_id(drone_id: str):
     '''
     Deletes the drone with the given drone_id.
     '''
-    change('DELETE FROM drone WHERE drone_id=:drone_id',{'drone_id': drone_id})
+    change('DELETE FROM drone WHERE drone_id=:drone_id', {'drone_id': drone_id})
+
 
 def get_discord_id_of_drone(drone_id: str) -> str:
     '''
