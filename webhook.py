d8af6d5019d684660f80e2a5ca3411f98d461f42
HexCorpDrone <hexcorpdrone@gmail.com>
2020-07-25 19:48:04 +0100

    webhook is webfuckinsexy. and that should be it if im not mistaken

--- webhook.py
+++ webhook.py
@@ -2,11 +2,12 @@ import discord
 
 from bot_utils import get_id
 from channels import DRONE_HIVE_CHANNELS
-from roles import DRONE, ENFORCER_DRONE, has_role
+from roles import ENFORCER_DRONE, has_role
 
 from glitch import glitch_if_applicable
 
-from resources import *
+from resources import ENFORCER_AVATAR, DRONE_AVATAR
+
 
 async def send_webhook_with_specific_output(message: discord.Message, webhook: discord.Webhook, output):
     if message.channel.name in DRONE_HIVE_CHANNELS:
@@ -17,14 +18,16 @@ async def send_webhook_with_specific_output(message: discord.Message, webhook: d
     else:
         await webhook.send(glitch_if_applicable(output, message.author), username=message.author.display_name, avatar_url=message.author.avatar_url)
 
+
 async def send_webhook(message: discord.Message, webhook: discord.Webhook):
     await send_webhook_with_specific_output(message, webhook, message.content)
+
 
 async def get_webhook_for_channel(channel: discord.TextChannel) -> discord.Webhook:
     webhooks = await channel.webhooks()
     if len(webhooks) == 0:
-        #No webhook available, create one.
-        return_webhook = await channel.create_webhook(name = "AI Webhook")
+        # No webhook available, create one.
+        return_webhook = await channel.create_webhook(name="AI Webhook")
     else:
         return_webhook = webhooks[0]
 
