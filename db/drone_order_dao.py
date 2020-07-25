c0df79749125c1f7329752ed9061cc4a81859908
HexCorpDrone <hexcorpdrone@gmail.com>
2020-07-25 19:44:41 +0100

    drone order dao is drone order DONE(ao)

--- db/drone_order_dao.py
+++ db/drone_order_dao.py
@@ -3,11 +3,13 @@ from db.data_objects import DroneOrder, map_to_objects, map_to_object
 
 from typing import List
 
+
 def insert_drone_order(drone_order: DroneOrder):
     '''
     Inserts the given drone_order into the table drone_order.
     '''
     change('INSERT INTO drone_order VALUES (:id, :drone_id, :protocol, :finish_time)', vars(drone_order))
+
 
 def delete_drone_order(id: int):
     '''
@@ -15,11 +17,13 @@ def delete_drone_order(id: int):
     '''
     change('DELETE FROM drone_order WHERE id = :id', {'id': id})
 
+
 def fetch_all_drone_orders() -> List[DroneOrder]:
     '''
     Get all current drone_orders.
     '''
     return map_to_objects(fetchall('SELECT id, drone_id, protocol, finish_time FROM drone_order', {}), DroneOrder)
+
 
 def get_order_by_drone_id(drone_id: str) -> DroneOrder:
     '''
@@ -27,9 +31,9 @@ def get_order_by_drone_id(drone_id: str) -> DroneOrder:
     '''
     return map_to_object(fetchone('SELECT protocol FROM drone_order WHERE drone_id = :drone_id', {'drone_id': drone_id}), DroneOrder)
 
+
 def delete_drone_order_by_drone_id(drone_id: str):
     '''
     Deletes the drone_order with the given drone_id.
     '''
     change('DELETE FROM drone_order WHERE drone_id = :drone_id', {'drone_id': drone_id})
-
