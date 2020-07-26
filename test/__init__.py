9b3291602339af5d049b5dd1f19645e3bb91a0be
Drone #9813 <9813@hexcorp.net>
2020-07-26 11:25:42 +0200

    added a few more edge cases and comments to test

--- test/__init__.py
+++ test/__init__.py
@@ -5,6 +5,7 @@ import unittest
 class TestBotUtils(unittest.TestCase):
 
     def test_get_id(self):
+        # regular usage
         self.assertEqual(get_id('⬡-Drone #9813'), '9813')
         self.assertEqual(get_id('⬡-Drone #0006'), '0006')
         self.assertEqual(get_id('⬡-Drone #0024'), '0024')
@@ -13,6 +14,13 @@ class TestBotUtils(unittest.TestCase):
         self.assertEqual(get_id('⬡-Drone #5800'), '5800')
         self.assertEqual(get_id('⬡-Drone #5000'), '5000')
 
+        # ID too long -> gives first valid sequence
         self.assertEqual(get_id('ID too long 56789'), '5678')
 
+        # not a valid ID
         self.assertIsNone(get_id('NotADrone'))
+        self.assertIsNone(get_id('ID too short 123'))
+
+        # invalid inputs
+        self.assertRaises(TypeError, get_id, None)
+        self.assertRaises(TypeError, get_id, 9813)
