4612818df7c6aa594cf4c4769be317673b7a3662
HexCorpDrone <hexcorpdrone@gmail.com>
2020-07-31 20:17:42 +0100

    test Works but asserts nothing currently

--- ai/amplifier.py
+++ ai/amplifier.py
@@ -11,19 +11,27 @@ LOGGER = logging.getLogger('ai')
 def generate_amplification_information(target_channel, drones):
     LOGGER.info("Amplifying message.")
 
+    print(target_channel)
+    print(drones)
+
     for drone in drones:
+
+        print("Iterating")
 
         LOGGER.debug(f"Preparing drone {drone} for amplification.")
 
         if not re.match(r"\d{4}", drone):
+            print("Not a drone ID.")
             continue  # Skip any non-drone IDs.
 
         LOGGER.debug("Getting discord ID from database.")
         if (drone_from_db := get_discord_id_of_drone(drone)) is None:
+            print("Not found in DB.")
             continue  # Given drone does not exist on server.
 
         amplifier_drone = target_channel.guild.get_member(drone_from_db.id)
         if amplifier_drone is None:
+            print("Couldn't get member from guild.")
             continue  # If getting the member somehow failed (which it really shouldn't), keep calm and carry on.
 
         # Set the avatar URL as appropriate.
--- test/test_amplifier.py
+++ test/test_amplifier.py
@@ -1,29 +1,32 @@
 import discord
 import unittest
 import ai.amplifier as amp
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock, Mock
 from channels import REPETITIONS
 from db.data_objects import Drone
+
+mocked_drone = Drone(id="5890")
+
+mocked_member = discord.Member
+mocked_member.display_name = "heck drone 5890"
+
+test_guild = Mock()
+test_guild.get_member.return_value = mocked_member
+
+test_channel = PropertyMock()
+test_channel.guild = test_guild
+test_channel.name = REPETITIONS
 
 
 class AmplifierTest(unittest.TestCase):
 
-    mocked_drone = Drone(id="5890")
-    mocked_member = discord.Member
-    mocked_member.display_name = "heck drone 5890"
-
-    test_guild = discord.Guild(data={"id": 5890}, state=None)
-
-    test_channel = discord.TextChannel(guild=test_guild, state=None, data={"id": 5891, "type": "i 'unno", "name": REPETITIONS, "position": "way at the top"})
-
-    
-
     @patch("ai.amplifier.get_discord_id_of_drone", return_value=mocked_drone)
-    def test_amplifier_generator_returns_drones(self, mocked_dao, mocked_discord):
-        
+    @patch("ai.amplifier.has_role", return_value=False)
+    def test_amplifier_generator_returns_drones(self, mocked_dao, mocked_get):
+
         print("Boop beep.")
 
-        for amp_drone in amp.generate_amplification_information(AmplifierTest.test_channel, ["5890"]):
+        for amp_drone in amp.generate_amplification_information(test_channel, ["5890"]):
             print(amp_drone)
 
         print("Beep boop.")
