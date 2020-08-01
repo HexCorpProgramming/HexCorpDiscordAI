707d6093dfdf33d0b38dea3c885799cbab5c30cf
HexCorpDrone <hexcorpdrone@gmail.com>
2020-08-01 12:50:01 +0100

    id converter now uses sets to avoid duplicate parameters

--- id_converter.py
+++ id_converter.py
@@ -1,22 +1,22 @@
 import discord
 import re
 from db.drone_dao import get_discord_id_of_drone
-from typing import List, Optional
+from typing import Set, Optional
 
 
-def convert_ids_to_members(guild: discord.Guild, drone_ids) -> List[discord.Member]:
+def convert_ids_to_members(guild: discord.Guild, drone_ids) -> Set[discord.Member]:
     '''
     Takes a list of string IDs (e.g "5890", "9813") and returns a list of converted member objects.
     '''
 
-    drone_members = []
+    drone_members = set()
 
     for drone_id in drone_ids:
         drone_member = convert_id_to_member(guild, drone_id)
         print("Drone member is:")
         print(drone_member)
         if drone_member is not None:
-            drone_members.append(drone_member)
+            drone_members.add(drone_member)
 
     return drone_members
 
