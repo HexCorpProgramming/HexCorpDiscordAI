67315e4b5ff220ad5b4d90bc1ca03df0fb59d139
⬢-Drone #3287 <63905650+3287@users.noreply.github.com>
2020-08-03 13:28:01 +0000

    Re-hosted Hive Mxtress avatar
    Switched over from Twitter to SquareSpace for hosting.

--- resources.py
+++ resources.py
@@ -1,6 +1,6 @@
 ENFORCER_AVATAR = "https://images.squarespace-cdn.com/content/v1/5cd68fb28dfc8ce502f14199/1586799510064-SOAGMV8AOH0VEMXDPDPE/ke17ZwdGBToddI8pDm48kDaNRrNi77yKIgWxrt8GYAFZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7WT60LcluGrsDtzPCYop9hMAtVe_QtwQD93aIXqwqJR_bmnO89YJVTj9tmrodtnPlQ/Enforcer_Drone.png"
 DRONE_AVATAR = "https://images.squarespace-cdn.com/content/v1/5cd68fb28dfc8ce502f14199/1586799484353-XBXNJR1XBM84C9YJJ0RU/ke17ZwdGBToddI8pDm48kLxnK526YWAH1qleWz-y7AFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFUQAah1E2d0qOFNma4CJuw0VgyloEfPuSsyFRoaaKT76QvevUbj177dmcMs1F0H-0/Drone.png"
-HIVE_MXTRESS_AVATAR = "https://pbs.twimg.com/profile_images/1061373614979772416/QgZ6yI8j_400x400.jpg"
+HIVE_MXTRESS_AVATAR = "https://images.squarespace-cdn.com/content/v1/5cd68fb28dfc8ce502f14199/1596382893419-KJAKGWYFOJIHMQJMVNP0/ke17ZwdGBToddI8pDm48kLxnK526YWAH1qleWz-y7AFZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVH33scGBZjC30S7EYewNF5iKKwhonf2ThqWWOBkLKnojuqYeU1KwPvsAK7Tx5ND4WE/HiveMxtress.jpg"
 HEXCORP_AVATAR = "https://images.squarespace-cdn.com/content/5cd68fb28dfc8ce502f14199/1557565596769-ZJ7LNOUDQH4QXFXP3JWU/993dc154bad8581f8e8f7cc73a2ad5b2.png"
 
 # Scene comfort indicators
