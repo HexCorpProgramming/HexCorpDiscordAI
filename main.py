efdadcb80ec825302e07849486b10e3c07deccab
Drone #9813 <9813@hexcorp.net>
2020-10-16 07:08:03 +0200

    simplified sending DMs

--- main.py
+++ main.py
@@ -238,11 +238,10 @@ async def help(context):
         else:
             commands_card.add_field(name=command_name, value=command_description, inline=False)
 
-    dm_channel = await context.author.create_dm()
     # TODO: hidden until DroneOS is officially released
-    # await context.send(embed=droneOS_card)
-    await dm_channel.send(embed=commands_card)
-    await dm_channel.send(embed=Hive_Mxtress_card)
+    # await context.author.send(embed=droneOS_card)
+    await context.author.send(embed=commands_card)
+    await context.author.send(embed=Hive_Mxtress_card)
 
 
 @guild_only()
