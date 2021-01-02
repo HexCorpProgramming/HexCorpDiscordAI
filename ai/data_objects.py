507a9a997c42a2b4f134836150e4cd0bfaff7b13
HexDrone 5890 <hexcorpdrone@gmail.com>
2021-01-02 15:37:27 +0000

    Modified MessageCopy to include message attachments.

--- ai/data_objects.py
+++ ai/data_objects.py
@@ -3,8 +3,10 @@ class MessageCopy:
         self,
         content=None,
         display_name=None,
-        avatar_url=None
+        avatar_url=None,
+        attachments=None
     ):
         self.content = content
         self.display_name = display_name
         self.avatar_url = avatar_url
+        self.attachments = attachments
