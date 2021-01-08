779ad7f91c62054d2486b6a8f732075eb782b1e0
HexDrone 5890 <hexcorpdrone@gmail.com>
2021-01-08 13:53:37 +0000

    All speech codes type now recognized by module

--- ai/speech_optimization.py
+++ ai/speech_optimization.py
@@ -25,6 +25,10 @@ class StatusType(Enum):
     ADDRESS_BY_ID_INFORMATIVE = 5
     # "5890 :: 110 :: 9813 :: You are cute!"
 
+    INVALID_ID = 6
+    # Status with an ID belonging to a different drone.
+
+
 LOGGER = logging.getLogger('ai')
 
 code_map = {
@@ -128,13 +132,21 @@ code_map = {
 
 status_code_regex = re.compile(r'^((\d{4}) :: (\d{3}))( :: (.*))?$')
 '''
-Regex groups:
+Regex groups for full status code regex:
 0: Full match. (5890 :: 200 :: Additional information)
 1: Plain status code (5890 :: 200)
 2: Author's drone ID (5890)
 3: Status code (200)
-4: Informative status addition with double colon formatting (" :: Additional information")
+4: Informative status addition with double colon (" :: Additional information")
 5: Informative status text. ("Additional information")
+'''
+addressing_regex = re.compile(r'(\d{4})( :: (.*))?')
+'''
+This regex is to be checked on the status regex's 5th group when the status code is 110 (addressing).
+0: Full match ("9813 :: Additional information")
+1: ID of drone to address ("9813")
+2: Informative status text with double colon (" :: Additional information")
+3: Informative status text ("Additional information")
 '''
 
 CHANNEL_BLACKLIST = [ORDERS_REPORTING, ORDERS_COMPLETION, MODERATION_CHANNEL, MODERATION_LOG]
@@ -151,28 +163,6 @@ def get_acceptable_messages(author, channel):
         ]
     else:
         return []
-
-
-def status_correctly_identified(author: discord.Author, plain_status=None, informative_status=None):
-    '''
-    Returns true if status begins with author's drone ID. Otherwise returns false.
-
-    Author: 5890
-    Message: '5890 :: 200'
-    Returns: True
-    Message: '9813 :: 200'
-    Returns: False
-    '''
-
-    required_id = get_id(author.display_name)
-
-    if plain_status is not None:
-        return required_id == plain_status.group(1)
-    elif informative_status is not None:
-        return required_id == informative_status.group(1)
-    else:
-        # This shouldn't happen.
-        return False
 
 
 def translate_code(plain_status=None, informative_status=None, special_status=None):
@@ -194,16 +184,33 @@ def translate_code(plain_status=None, informative_status=None, special_status=No
 
 
 def get_status_type(status: Optional[re.Match]):
+    '''
+    Identifying if a status is ADDRESS_BY_ID_PLAIN or INFORMATIVE:
+    An "address by ID" status must always use the "informative" field even if plain
+    in order to specify a target drone ID.
+    - If the informative field exactly matches 4 digits, it's a plain status code.
+    - Otherwise, it's an informative code.
+    '''
+
     if status is None:
         return StatusType.NONE
-    elif status.group(3) == "110" and status.group(4) is None:
-        return StatusType.ADDRESS_PLAIN
-    elif status.group(3) == "110" and status.group(4) is not None:
-        return StatusType.ADDRESS_INFORMATIVE
-    elif status.group(4) is not None:
+    elif status.group(3) == "110" and status.group(5) is not None:
+        # Handle 110 address-by-ID special case.
+        address_info = addressing_regex.match(status.group(5))
+        if address_info.group(1) is not None and address_info.group(2) is not None:
+            return StatusType.ADDRESS_BY_ID_INFORMATIVE
+        elif address_info.group(1) is not None and address_info.group(2) is None:
+            return StatusType.ADDRESS_BY_ID_PLAIN
+        else:
+            # If a target ID is not found then it's just an informative status code.
+            return StatusType.INFORMATIVE
+    elif status.group(5) is not None:
         return StatusType.INFORMATIVE
-    else:
+    elif status.group(5) is None:
         return StatusType.PLAIN
+    else:
+        return StatusType.NONE
+    
 
 async def optimize_speech(message: discord.Message, message_copy):
     '''
@@ -223,6 +230,17 @@ async def optimize_speech(message: discord.Message, message_copy):
 
     # Determine type type
     status_type = get_status_type(status)
+
+    if status_type is StatusType.PLAIN:
+        LOGGER.info("PLAIN STATUS CODE GOT")
+    if status_type is StatusType.INFORMATIVE:
+        LOGGER.info("INFORMATIVE STATUS CODE GOT")
+    if status_type is StatusType.NONE:
+        LOGGER.info("NO STATUS TYPE RECIEVED")
+    if status_type is StatusType.ADDRESS_BY_ID_INFORMATIVE:
+        LOGGER.info("INFORMATIVE ID ADDRESS GOT")
+    if status_type is StatusType.ADDRESS_BY_ID_PLAIN:
+        LOGGER.info("PLAIN ID ADDRESS GOT")
 
 '''
 TODO:
