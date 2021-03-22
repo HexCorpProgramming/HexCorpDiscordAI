5de5cbf958e9263ca94558f6129403cf688c49f7
Drone #9813 <9813@hexcorp.net>
2021-03-22 09:13:07 +0100

    fixed a reference error with messages in temporary dronification

--- ai/temporary_dronification.py
+++ ai/temporary_dronification.py
@@ -68,7 +68,7 @@ class TemporaryDronificationCog(Cog):
                 matching_request = request
                 break
 
-        if matching_request and message.reference.resolved == matching_request.question_message:
+        if matching_request and message.reference and message.reference.resolved == matching_request.question_message:
             LOGGER.info(f"Message detected as response to a temporary dronification request {matching_request}")
             if message.content.lower() == "y".lower():
                 LOGGER.info("Consent given for temporary dronification. Changing roles and writing to DB...")
