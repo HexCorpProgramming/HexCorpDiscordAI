a625e201bb8afbed848238507c220a6789ece6f5
HexDrone 5890 <hexcorpdrone@gmail.com>
2021-03-25 14:18:47 +0000

    battery module now tracks and deincrements battery

--- ai/battery.py
+++ ai/battery.py
@@ -1,51 +1,55 @@
-import copy
 from bot_utils import get_id
 from discord.ext import tasks
-from db.drone_dao import is_battery_powered
+from db.drone_dao import is_drone, is_battery_powered, deincrement_battery_minutes_remaining, get_battery_minutes_remaining
+from id_converter import convert_id_to_member
+import logging
 
 MAX_BATTERY_CAPACITY_HOURS = 8
-MAX_BATTERY_CAPACITY_MINS = MAX_BATTERY_CAPACITY_HOURS * 60
+MAX_BATTERY_CAPACITY_MINS = 480
 
-draining_batteries = {}
+draining_batteries = {}  # {drone_id: minutes of drain left}
+
+LOGGER = logging.getLogger('ai')
 
 
-async def drain_battery(message, message_copy=None):
+async def start_battery_drain(message, message_copy=None):
     '''
     If message author has battery DroneOS config enabled, begin or restart
     the task to drain 1 minute of battery per minute, for 15 minutes.
     '''
 
-    if not is_battery_powered(message.author):
+    if not is_drone(message.author) or not is_battery_powered(message.author):
         return False
 
     drone_id = get_id(message.author.display_name)
-    
-    if draining_batteries.get(drone_id) is None:
-        print("No drain record found. Starting task.")
-        drain_battery_task = copy.copy(drain_battery_source)
-        drain_battery_task.start(drone_id)
-        draining_batteries[drone_id] = drain_battery_task
-    else:
-        draining_batteries[drone_id].current_loop = 0
+    draining_batteries[drone_id] = 15
 
 
-@tasks.loop(seconds=3, count=5)
-async def drain_battery_source(drone):
-    '''
-    This task should not be started directly. Make a copy of it and start that
-    instead, so that multiple drones can be tracked as their batteries are drained.
-    '''
-    print(f"Draining battery of {drone}")
+@tasks.loop(seconds=1)
+async def track_active_battery_drain(bot):
+    LOGGER.info("Draining battery from active drones.")
+
+    inactive_drones = []
+
+    for drone, remaining_minutes in draining_batteries.items():
+        if remaining_minutes == 0:
+            LOGGER.info(f"Drone {drone} has been idle for 15 minutes. No longer draining power.")
+            # Cannot alter list while iterating, so add drone to list of drones to pop after the loop.
+            inactive_drones.append(drone)
+        else:
+            print(f"Draining 1 minute worth of charge from {drone}")
+            draining_batteries[drone] = remaining_minutes - 1
+            deincrement_battery_minutes_remaining(convert_id_to_member(bot.guilds[0], drone))
+
+    for inactive_drone in inactive_drones:
+        LOGGER.info(f"Removing {inactive_drone} from drain list.")
+        draining_batteries.pop(inactive_drone)
 
 
-@drain_battery_source.after_loop
-async def drain_battery_finished():
-    print("Finished battery drain.")
+@tasks.loop(minutes=1)
+async def track_drained_batteries(bot):
+    LOGGER.info("Tracking drained batteries.")
 
-
-@drain_battery_source.before_loop
-async def drain_battery_begin():
-    print("Beginning battery drain.")
 
 async def recharge_battery(drone):
     '''
@@ -55,7 +59,7 @@ async def recharge_battery(drone):
     return
 
 
-async def append_battery_indicator():
+async def append_battery_indicator(message, message_copy):
     '''
     Appends battery icon to messages either by prepending message content,
     or by appending display name. (e.g ⬡-Drone #0001 [|||]-)
