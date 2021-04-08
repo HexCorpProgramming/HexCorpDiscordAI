6f2401d6711d57655134e0406491e018c1d03d01
HexDrone 5890 <hexcorpdrone@gmail.com>
2021-04-08 13:06:45 +0100

    made new .py file for emojis similar to roles.py

---
+++ emoji.py
@@ -0,0 +1,7 @@
+# HexCorp Discord emoji names
+
+BATTERY_FULL = "hexbatteryfull"
+BATTERY_MID = "hexbatterymedium"
+BATTERY_LOW = "hexbatterylow"
+BATTERY_EMPTY = "hexbatteryempty"
+DRONE_EMOJI = "hexdroneemoji"
