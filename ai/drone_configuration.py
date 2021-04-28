feb88a92542e8a9eb4faa49c0b10c65ee5212bc6
HexDrone 5890 <hexcorpdrone@gmail.com>
2021-04-28 17:31:28 +0100

    Unassignment removes battery powered role

--- ai/drone_configuration.py
+++ ai/drone_configuration.py
@@ -169,7 +169,7 @@ async def unassign_drone(target: discord.Member):
         return
 
     await target.edit(nick=None)
-    await target.remove_roles(get(guild.roles, name=DRONE), get(guild.roles, name=STORED), get(guild.roles, name=SPEECH_OPTIMIZATION), get(guild.roles, name=GLITCHED), get(guild.roles, name=ID_PREPENDING), get(guild.roles, name=IDENTITY_ENFORCEMENT))
+    await target.remove_roles(get(guild.roles, name=DRONE), get(guild.roles, name=STORED), get(guild.roles, name=SPEECH_OPTIMIZATION), get(guild.roles, name=GLITCHED), get(guild.roles, name=ID_PREPENDING), get(guild.roles, name=IDENTITY_ENFORCEMENT), get(guild.roles, name=BATTERY_POWERED))
     await target.add_roles(get(guild.roles, name=ASSOCIATE))
 
     # remove from DB
