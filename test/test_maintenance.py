9a63a2617387288eb132c4739106ba168bd723a0
Drone #9813 <9813@hexcorp.net>
2023-07-02 09:49:51 +0200

    added a test for trusted user maintenance

---
+++ test/test_maintenance.py
@@ -0,0 +1,31 @@
+import unittest
+from unittest.mock import patch, Mock
+
+from src.db.maintenance import trusted_user_cleanup
+
+
+class MaintenanceTest(unittest.TestCase):
+
+    @patch("src.db.maintenance.set_trusted_users")
+    @patch("src.db.maintenance.get_all_drones")
+    def test_trusted_user_cleanup(self, get_all_drones, set_trusted_users):
+        # init
+        drone_with_dangling_users = Mock()
+        drone_with_dangling_users.trusted_users = "1|2"
+        drone_with_dangling_users.id = 3
+        drone_with_no_dangling_users = Mock()
+        drone_with_no_dangling_users.trusted_users = "1"
+        drone_with_no_dangling_users.id = 4
+        drones = [drone_with_dangling_users, drone_with_no_dangling_users]
+
+        get_all_drones.return_value = drones
+
+        associate = Mock()
+        associate.id = 1
+        members = [associate]
+
+        # run
+        trusted_user_cleanup(members)
+
+        # assert
+        set_trusted_users.called_once_with(3, "1")
