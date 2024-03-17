b32bcbd3a59d8995f793203ce36817300d229360
Drone #9813 <9813@hexcorp.net>
2024-03-17 11:57:13 +0100

    documentation

--- src/ai/mantra.py
+++ src/ai/mantra.py
@@ -10,15 +10,26 @@ from src.db.drone_dao import (get_battery_minutes_remaining,
 from src.resources import MAX_BATTERY_CAPACITY_MINS
 
 mantra_counters: Dict[str, int] = {}
+'''
+Maps drone IDs to the current mantra repetition step that drone is at.
+'''
 
 
 async def check_for_mantra(message: discord.Message, message_copy=None):
+    '''
+    Checks if a message should be counted for mantra repetition.
+    '''
+
     code_match = status_code_regex.match(message.content)
     if code_match and message.channel.name == 'hive-repetitions' and is_battery_powered(message.author):
         await handle_mantra(message, code_match)
 
 
 async def handle_mantra(message: discord.Message, code_match: Match):
+    '''
+    Keeps track of the mantra repetitions a drone has done.
+    '''
+
     drone_id = get_id(message.author.display_name)
 
     if drone_id not in mantra_counters and code_match.group(3) == "301":
@@ -36,6 +47,10 @@ async def handle_mantra(message: discord.Message, code_match: Match):
 
 
 async def increase_battery_by_five_percent(message: discord.Message):
+    '''
+    Increases the battery of the given drone by 5 percent capping at 100% capacity.
+    Acknowledges the mantra repetitions by sending a message in the mantra channel as well.
+    '''
     minutes_remaining = get_battery_minutes_remaining(message.author)
     if minutes_remaining >= MAX_BATTERY_CAPACITY_MINS:
         await message.channel.send("Good drone. Battery already at 100%.")
