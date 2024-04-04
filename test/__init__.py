3c84bf80cb594333e9ce101de51ad42d8e04e952
Zebra North <mrzebra@mrzebra.co.uk>
2024-04-04 07:51:04 +0000

    Prepare test DB

--- test/__init__.py
+++ test/__init__.py
@@ -1,11 +1,13 @@
 import asyncio
 from pathlib import Path
 import src.db.database
-from main import prepare_database
 
 # Make database.connect() default to 'test.db'.
 original_connect = src.db.database.connect
 src.db.database.connect = lambda filename='test.db': original_connect(filename=filename)
+
+# The prepare_database function can only be imported after connect() is patched.
+from main import prepare_database  # noqa: E402
 
 # Reset the database.
 p = Path('test.db')
