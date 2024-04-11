4f28307e221e38fe3250dfa4b6fa7edd58cc8793
Zebra North <mrzebra@mrzebra.co.uk>
2024-04-11 12:07:40 +0000

    Add third person identity enforcement

--- test/test_display_names.py
+++ test/test_display_names.py
@@ -10,7 +10,8 @@ class TestDisplayNames(IsolatedAsyncioTestCase):
     @patch("src.display_names.is_glitched", return_value=False)
     @patch("src.display_names.is_optimized", return_value=False)
     @patch("src.display_names.is_prepending_id", return_value=False)
-    async def test_display_name_returns_false_when_unoptimized_drone_require_no_updates(self, glitched, optimized, prepending, identity_enforced, battery_powered):
+    @patch("src.display_names.is_third_person_enforced", return_value=False)
+    async def test_display_name_returns_false_when_unoptimized_drone_require_no_updates(self, third_person, glitched, optimized, prepending, identity_enforced, battery_powered):
 
         mock_user = AsyncMock()
         mock_user.display_name = "⬡-Drone #5890"
@@ -22,7 +23,8 @@ class TestDisplayNames(IsolatedAsyncioTestCase):
     @patch("src.display_names.is_glitched", return_value=False)
     @patch("src.display_names.is_optimized", return_value=True)
     @patch("src.display_names.is_prepending_id", return_value=False)
-    async def test_display_name_returns_false_when_optimized_drone_require_no_updates(self, glitched, optimized, prepending, identity_enforced, battery_powered):
+    @patch("src.display_names.is_third_person_enforced", return_value=False)
+    async def test_display_name_returns_false_when_optimized_drone_require_no_updates(self, third_person, prepending, optimized, glitched, identity_enforced, battery_powered):
 
         mock_user = AsyncMock()
         mock_user.display_name = "⬢-Drone #5890"
@@ -34,19 +36,28 @@ class TestDisplayNames(IsolatedAsyncioTestCase):
     @patch("src.display_names.is_glitched", return_value=False)
     @patch("src.display_names.is_optimized", return_value=True)
     @patch("src.display_names.is_prepending_id", return_value=False)
-    async def test_display_name_returns_true_and_edits_nick_of_unoptimized_drone_when_updates_required(self, glitched, optimized, prepending, identity_enforced, battery_powered):
+    @patch("src.display_names.is_third_person_enforced", return_value=False)
+    async def test_display_name_returns_true_and_edits_nick_of_unoptimized_drone_when_updates_required(self, third_person, glitched, optimized, prepending, identity_enforced, battery_powered):
+        options = third_person, glitched, optimized, prepending, identity_enforced, battery_powered
 
-        mock_user = AsyncMock()
-        mock_user.display_name = "⬡-Drone #5890"
-        self.assertTrue(await update_display_name(mock_user))
-        mock_user.edit.assert_called_with(nick="⬢-Drone #5890")
+        # Test each option individually.
+        for option in options:
+            # Set all options to False except one.
+            for o in options:
+                o.return_value = o == option
+
+            mock_user = AsyncMock()
+            mock_user.display_name = "⬡-Drone #5890"
+            self.assertTrue(await update_display_name(mock_user))
+            mock_user.edit.assert_called_with(nick="⬢-Drone #5890")
 
     @patch("src.display_names.is_battery_powered", return_value=False)
     @patch("src.display_names.is_identity_enforced", return_value=False)
     @patch("src.display_names.is_glitched", return_value=False)
     @patch("src.display_names.is_optimized", return_value=False)
     @patch("src.display_names.is_prepending_id", return_value=False)
-    async def test_display_name_returns_true_and_edits_nick_of_optimized_drone_when_updates_required(self, glitched, optimized, prepending, identity_enforced, battery_powered):
+    @patch("src.display_names.is_third_person_enforced", return_value=False)
+    async def test_display_name_returns_true_and_edits_nick_of_optimized_drone_when_updates_required(self, third_person, glitched, optimized, prepending, identity_enforced, battery_powered):
 
         mock_user = AsyncMock()
         mock_user.display_name = "⬢-Drone #5890"
---
+++ test/test_third_person_enforcement.py
@@ -0,0 +1,63 @@
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import Mock, patch
+from src.ai.third_person_enforcement import enforce_third_person, replace_third_person
+
+
+class ThirdPersonEnforcementTest(IsolatedAsyncioTestCase):
+
+    async def test_replace_third_person(self) -> None:
+        '''
+        Ensure that first person pronouns are replaced correctly.
+        '''
+
+        cases = [
+            ('', ''),
+            ('hello world', 'hello world'),
+            ('I am a drone', 'It is a drone'),
+            ('a drone am I', 'a drone is it'),
+            ('I am Drone.  I am Drone.', 'It is Drone.  It is Drone.'),
+            ('i am i', 'It is it'),
+            ('I am just me', 'It is just it'),
+            ('I am by myself', 'It is by itself'),
+            ('Me, myself and I', 'It, itself and it'),
+            ('That is mine', 'That is its'),
+            ('I\'ve got mine', 'It\'s got its'),
+            ('Myself and myself or my', 'Itself and itself or its'),
+            ('test! I. I? I@ I', 'test! It. It? It@ it'),
+            ('I.I.I', 'It.It.It'),
+            ('I. I. I', 'It. It. It'),
+            ('I\'d do what i\'d do', 'It\'d do what it\'d do'),
+
+            # These cases could be improved.
+            ('Do I just do what I like?', 'Do it just do what it like?'),
+            ('I have to, have I?', 'It have to, have it?'),
+        ]
+
+        for text, expected in cases:
+            self.assertEqual(replace_third_person(text), expected)
+
+    @patch('src.ai.third_person_enforcement.third_person_enforcable', return_value=True)
+    async def test_enforce_third_person(self, third_person_enforcable) -> None:
+        '''
+        Ensure that messages are altered if enforcement is enabled.
+        '''
+
+        message = Mock()
+        message_copy = Mock(content='I am a drone')
+
+        await enforce_third_person(message, message_copy)
+
+        self.assertEqual('It is a drone', message_copy.content)
+
+    @patch('src.ai.third_person_enforcement.third_person_enforcable', return_value=False)
+    async def test_enforce_third_person_off(self, third_person_enforcable) -> None:
+        '''
+        Ensure that messages are not altered if enforcement is disabled.
+        '''
+
+        message = Mock()
+        message_copy = Mock(content='I am a drone')
+
+        await enforce_third_person(message, message_copy)
+
+        self.assertEqual('I am a drone', message_copy.content)
