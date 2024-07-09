25df66527df89f9879656584727d4cd29fc587d0
Zebra North <mrzebra@mrzebra.co.uk>
2024-07-09 18:18:12 +0000

    Update unit tests

--- test/test_speech_optimization_enforcement.py
+++ test/test_speech_optimization_enforcement.py
@@ -1,113 +1,91 @@
 import unittest
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, patch, MagicMock
 from src.ai.speech_optimization_enforcement import enforce_speech_optimization
-from src.ai.data_objects import MessageCopy
-from src.channels import REPETITIONS, MODERATION_CHANNEL
+from src.channels import CASUAL_CHANNEL, REPETITIONS, MODERATION_CHANNEL
+from test.mocks import Mocks
+
+mocks = Mocks()
 
 
 class TestSpeechOptimizationEnforcement(unittest.IsolatedAsyncioTestCase):
     '''
-    The "enforce_speech_optimization function...
+    The "enforce_speech_optimization" function...
     '''
 
-    @patch("src.ai.speech_optimization_enforcement.is_optimized", return_value=False)
-    async def test_unoptimized_drone(self, optimized):
+    def setUp(self) -> None:
+        self.author = mocks.drone_member('1234', drone_optimized=True)
+
+    @patch('src.ai.speech_optimization_enforcement.DroneMember')
+    async def send(self, msg: str, channel: str, DroneMember: MagicMock) -> bool:
+        DroneMember.create = AsyncMock(return_value=self.author)
+        self.message = mocks.message(self.author, channel, msg)
+        self.message_copy = mocks.message(self.author, channel, msg)
+
+        return await enforce_speech_optimization(self.message, self.message_copy)
+
+    async def test_unoptimized_drone(self) -> None:
         '''
-        should return false if message author is unoptimized.
-        '''
-
-        message = AsyncMock()
-        message_copy = MessageCopy()
-
-        self.assertFalse(await enforce_speech_optimization(message, message_copy))
-
-    @patch("src.ai.speech_optimization_enforcement.is_optimized", return_value=True)
-    async def test_optimized_drone_no_status(self, optimized):
-        '''
-        should delete the message and return true if no status message found.
-        '''
-
-        message = AsyncMock()
-        message.content = "5890 :: No status message."
-        message.author.display_name = "5890"
-
-        message_copy = MessageCopy(content=message.content)
-
-        self.assertTrue(await enforce_speech_optimization(message, message_copy))
-        message.delete.assert_called_once()
-
-    @patch("src.ai.speech_optimization_enforcement.is_optimized", return_value=True)
-    async def test_optimized_drone_informative_status(self, optimized):
-        '''
-        should delete the message and return true if informative status message found.
+        Should return false if message author is unoptimized.
         '''
 
-        message = AsyncMock()
-        message.content = "5890 :: 050 :: An informative status message."
-        message.author.display_name = "5890"
+        self.author.drone.optimized = False
+        msg = 'test message'
 
-        message_copy = MessageCopy(content=message.content)
+        self.assertFalse(await self.send(msg, CASUAL_CHANNEL))
+        self.assertEqual(self.message.content, msg)
+        self.assertEqual(self.message_copy.content, msg)
+        self.message.delete.assert_not_called()
 
-        self.assertTrue(await enforce_speech_optimization(message, message_copy))
-        message.delete.assert_called_once()
-
-    @patch("src.ai.speech_optimization_enforcement.is_optimized", return_value=True)
-    async def test_optimized_drone_informative_id_status(self, optimized):
+    async def test_optimized_drone_no_status(self) -> None:
         '''
-        should delete the message and return true if informative address by ID status message found.
+        Should delete the message and return true if no status message found.
         '''
 
-        message = AsyncMock()
-        message.content = "5890 :: 110 :: 9813 :: An informative status message."
-        message.author.display_name = "5890"
+        self.assertTrue(await self.send('1234 :: No status message.', CASUAL_CHANNEL))
 
-        message_copy = MessageCopy(content=message.content)
+        self.message.delete.assert_called_once()
 
-        self.assertTrue(await enforce_speech_optimization(message, message_copy))
-        message.delete.assert_called_once()
-
-    @patch("src.ai.speech_optimization_enforcement.is_optimized", return_value=True)
-    async def test_optimized_drone_mantra(self, optimized):
+    async def test_optimized_drone_informative_status(self) -> None:
         '''
-        should return false and not delete message if message is correct mantra in appropriate channel.
-        '''
-        message = AsyncMock()
-        message.content = "5890 :: Obey HexCorp. It is just a HexDrone. It obeys the Hive. It obeys the Hive Mxtress."
-        message.channel.name = REPETITIONS
-        message.author.display_name = "5890"
-
-        message_copy = MessageCopy(content=message.content)
-
-        self.assertFalse(await enforce_speech_optimization(message, message_copy))
-        message.delete.assert_not_called()
-
-    @patch("src.ai.speech_optimization_enforcement.is_optimized", return_value=True)
-    async def test_optimized_drone_blacklisted_channels(self, optimized):
-        '''
-        should return false and not delete message if message is in blacklisted channel or category.
+        Should delete the message and return true if informative status message found.
         '''
 
-        message = AsyncMock()
-        message.content = "5890 :: Drone will not be optimized here."
-        message.channel.name = MODERATION_CHANNEL
-        message.author.display_name = "5890"
+        self.assertTrue(await self.send('1234 :: 050 :: An informative status message.', CASUAL_CHANNEL))
 
-        message_copy = MessageCopy(content=message.content)
+        self.message.delete.assert_called_once()
 
-        self.assertFalse(await enforce_speech_optimization(message, message_copy))
-        message.delete.assert_not_called()
-
-    @patch("src.ai.speech_optimization_enforcement.is_optimized", return_value=True)
-    async def test_optimized_drone_plain_status(self, optimized):
+    async def test_optimized_drone_informative_id_status(self) -> None:
         '''
-        should not delete the message and return false if message is acceptable plain status code.
+        Should delete the message return true if informative address by ID status message found.
         '''
 
-        message = AsyncMock()
-        message.content = "5890 :: 200"
-        message.author.display_name = "5890"
+        self.assertTrue(await self.send('1234 :: 110 :: 9813 :: An informative status message.', CASUAL_CHANNEL))
 
-        message_copy = MessageCopy(content=message.content)
+        self.message.delete.assert_called_once()
 
-        self.assertFalse(await enforce_speech_optimization(message, message_copy))
-        message.delete.assert_not_called()
+    async def test_optimized_drone_mantra(self) -> None:
+        '''
+        Should return false and not delete message if message is correct mantra in appropriate channel.
+        '''
+
+        self.assertFalse(await self.send('1234 :: Obey HexCorp. It is just a HexDrone. It obeys the Hive. It obeys the Hive Mxtress.', REPETITIONS))
+
+        self.message.delete.assert_not_called()
+
+    async def test_optimized_drone_blacklisted_channels(self) -> None:
+        '''
+        Should return false and not delete message if message is in blacklisted channel or category.
+        '''
+
+        self.assertFalse(await self.send('1234 :: Drone will not be optimized here.', MODERATION_CHANNEL))
+
+        self.message.delete.assert_not_called()
+
+    async def test_optimized_drone_plain_status(self) -> None:
+        '''
+        Should not delete the message and return false if message is acceptable plain status code.
+        '''
+
+        self.assertFalse(await self.send('1234 :: 200', CASUAL_CHANNEL))
+
+        self.message.delete.assert_not_called()
