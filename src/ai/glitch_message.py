c2435f195b2248637355b38c743bcb4f8fd8a35a
Zebra North <mrzebra@mrzebra.co.uk>
2024-10-26 14:42:47 +0000

    Adjust log level

--- src/ai/glitch_message.py
+++ src/ai/glitch_message.py
@@ -133,7 +133,7 @@ async def glitch_if_applicable(message: discord.Message, message_copy: MessageCo
     elif member.drone.is_battery_powered and member.drone.get_battery_percent_remaining() < 30:
         glitch_amount = (MAX_GLITCH_AMOUNT - member.drone.get_battery_percent_remaining()) * 2
     else:
-        log.info("Not glitching message (drone is neither glitched nor low battery).")
+        log.debug("Not glitching message (drone is neither glitched nor low battery).")
         return False
 
     log.info(f"Glitching message for {message.author.display_name}, glitch amount: {glitch_amount}")
